54e27b3da594d120197c87691b07c9477a6e182e
Mitchelle-6022 <gichimuwamuyu@gmail.com>
2025-06-23 10:06:24 +0300

    first commit

---
+++ src/components/Courses.vue
@@ -0,0 +1,92 @@
+<script setup>
+import {ref} from 'vue'
+const courses= ref([
+    {
+        name:"Computer Science",
+        school:" SCI",
+        description:" Best course for computer enthusiasts",
+        intake:"July 2025"
+    },
+    {
+    name:"Medicine",
+        school:"School of Health Science",
+        description:"Best course for learning about health",
+        intake:"Sep 2025"
+    },
+    {
+        name:"BBIT",
+        school:"SCI",
+        description:" BEST for IT course",
+        intake:"May 2025"
+    },
+    {
+        name:"BIT",
+        school:"SCI",
+        description:"Best for ICT",
+        intake:"January 2025"
+    },
+    {
+name:"Education",
+        school:"BED",
+        description:"Best for teaching courses",
+        intanke:"Sep 2025"
+    },
+   {
+name:"Journalism",
+        school:"BED",
+        description:"Best for media courses",
+        intake:"Sep 2025"
+    },  
+     {
+name:"Clinical medicine",
+        school:"BED",
+        description:"Best for health courses",
+        intake:"Sep 2025"
+    },
+  {
+name:"Engineering",
+        school:"BED",
+        description:"Best for engineering courses",
+        intake:"Sep 2025"
+    },   
+   {
+name:"pyscologogy",
+        school:"BED",
+        description:"Best for health courses",
+        intake:"Sep 2025"
+    },  
+ {
+name:"Animal Health",
+        school:"BED",
+        description:"Best for vetinary courses",
+        intake:"Sep 2025"
+    },
+])
+</script>
+
+<template>
+   <v-container>
+   </v-container>
+    <v-row>
+
+    
+        <v-col md="6" v-for="course in courses">
+             <v-card color = "purple">
+        <v-card-item>
+          <v-card-title>{{course.name}}</v-card-title>
+
+          <v-card-subtitle>{{course.school}}</v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+        {{course.description}}
+        </v-card-text>
+        <v-card-text>
+            {{ course.intake }}
+        </v-card-text>
+      </v-card>
+
+        </v-col>
+        </v-row>
+
+</template>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,101 @@
+<script setup>
+
+</script>
+<template>
+  <!-- rotating images -->
+    <v-carousel>
+  <v-carousel-item
+    src="../../public/images/andrey-zvyagintsev-Eyg5Z7ARjWk-unsplash (3).jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="../../public/images/erika-fletcher-2Z0yN0Dhomw-unsplash.jpg"
+    cover
+ ></v-carousel-item>
+
+  <v-carousel-item
+    src="../../public/images/trnava-university-k8XfWMG-a0U-unsplash.jpg"
+    cover
+  ></v-carousel-item>
+  <v-carousel-item
+    src="../../public/images/dhiraj-chouhan-_1c5jIwOFUY-unsplash.jpg"
+    cover
+  ></v-carousel-item>
+</v-carousel>
+
+<!-- latest from jitegemee -->
+
+  <v-container>
+ <v-row>
+    <v-col cols="12" md="4">
+      <v-card
+        subtitle="This is a card subtitle"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
+        title="This is a title"
+      ></v-card>
+
+      <div class="text-center text-caption">Using Props Only</div>
+    </v-col>
+
+    <v-col cols="12" md="4">
+      <v-card>
+        <template v-slot:title>
+          This is a title
+        </template>
+
+        <template v-slot:subtitle>
+          This is a card subtitle
+        </template>
+
+        <template v-slot:text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+        </template>
+      </v-card>
+
+      <div class="text-center text-caption">Using Slots Only</div>
+    </v-col>
+
+    <v-col cols="12" md="4">
+      <v-card>
+        <v-card-item>
+          <v-card-title>This is a title</v-card-title>
+
+          <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+        </v-card-text>
+      </v-card>
+
+      <div class="text-center text-caption">Using Markup Only</div>
+    </v-col>
+  </v-row>
+   
+  </v-container>
+  <!-- partners -->
+
+   <v-container>
+   <v-row>
+    <v-col cols="12" md="2"> 
+      <v-img src="../../public/images/safaricom-logo-green.png"></v-img>
+    </v-col>
+    <v-col cols="12" md="2">
+      <v-img src="../../public/images/skyworth.png"></v-img>
+    </v-col>
+    <v-col cols="12" md="2">
+      <v-img src="../../public/images/oraimo.png"></v-img>
+    </v-col>
+    <v-col cols="12" md="2">
+      <v-img src="../../public/images/Hisia-Company-Logo-AI-File-Rectified-03-2.png"></v-img>
+    </v-col>
+    <v-col cols="12" md="2">
+      <v-img src="../../public/images/wikipedia-wordmark-en.svg"></v-img>
+    </v-col>
+    <v-col cols="12" md="2">
+      <v-img src="../../public/images/safaricom-logo-green.png"></v-img>
+    </v-col>
+  </v-row>
+   </v-container>
+</template>
